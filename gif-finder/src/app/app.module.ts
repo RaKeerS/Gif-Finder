6cdb6f06e6f0f44d21f49e311ba83915feb78a3e
RaKeerS <n.srikar22@gmail.com>
2021-05-27 19:45:45 +0530

    Did UI Design Configuration

--- gif-finder/src/app/app.module.ts
+++ gif-finder/src/app/app.module.ts
@@ -4,13 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { NzButtonModule } from 'ng-zorro-antd/button';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NzButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
