ac3ff8fcfdf61ec1ab6cb6868fa15e08db384228
RaKeerS <n.srikar22@gmail.com>
2021-05-30 23:09:11 +0530

    Establishing connection and fetched Data using API successful. Showing some results on first load, pagination works! However, need to fix caching!!

--- gif-finder/src/app/app.module.ts
+++ gif-finder/src/app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -7,6 +6,12 @@ import { AppComponent } from './app.component';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NgZorroAntdModule } from './ng-zorro-antd/ng-zorro-antd.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { NgxLiquidCacheModule, LiquidCacheConfig, LiquidCacheStorageTypes } from 'ngx-liquid-cache';
+
+import { FormsModule } from '@angular/forms';
 
 import { registerLocaleData } from '@angular/common'; 
 import en from '@angular/common/locales/en';
@@ -18,14 +23,19 @@ import * as AllIcons from '@ant-design/icons-angular/icons';
 
 import { HomePageComponent } from './home-page/home-page.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-import { HttpClientModule } from '@angular/common/http';
+
 
 registerLocaleData(en);
 
 const antDesignIcons = AllIcons as {
   [key: string]: IconDefinition;
 };
-const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key])
+const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesignIcons[key]);
+
+const liquidCacheConfig: LiquidCacheConfig = {
+  duration: 60,
+  storageType: LiquidCacheStorageTypes.localStorage
+};
 
 
 @NgModule({
@@ -35,12 +45,14 @@ const icons: IconDefinition[] = Object.keys(antDesignIcons).map(key => antDesign
     PageNotFoundComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule,
     AppRoutingModule,
     NzButtonModule,
     NgZorroAntdModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    NgxLiquidCacheModule.forRoot(liquidCacheConfig),
   ],
   providers: [ { provide: NZ_I18N, useValue: en_US }, { provide: NZ_ICONS, useValue: icons } ],
   bootstrap: [AppComponent]
--- gif-finder/src/app/home-page/home-page.component.ts
+++ gif-finder/src/app/home-page/home-page.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
+import { GiphyFetch } from '@giphy/js-fetch-api'
+
+import { LiquidCache } from 'ngx-liquid-cache';
+
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
@@ -7,9 +11,45 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
+  // API Key: q0CwrgPwQZWJmqjr9rDhKNYFVYDpojyh
+
+  public searchKeyword: string = "";
+  
+  public gifData: any;
+
+  readonly gf = new GiphyFetch('q0CwrgPwQZWJmqjr9rDhKNYFVYDpojyh');
+
   constructor() { }
 
   ngOnInit(): void {
+    this.initialLoading();
+  }
+
+  @LiquidCache('gifData1')
+  public async initialLoading() {
+    try {
+      this.gifData = await (await this.gf.trending({ limit: 24 })).data;
+      console.log(this.gifData);
+    } catch (error) {
+      console.log('Error occurred: ' + error);
+    }
+  }
+
+  public findGif(event: any) {
+
+  }
+
+  // Assuming to invoke getSingleUser(1), the result 
+  // will be stored in the cache system with key 'user1'
+  @LiquidCache('gifData{event}')
+  public async pageChange(event: any) {
+    console.log(event);
+    try {
+      this.gifData = await (await this.gf.trending({ limit: 24, offset: (((event - 1) * 24) + 1) })).data;
+      console.log(this.gifData);
+    } catch (error) {
+      console.log('Error occurred: ' + error);
+    }
   }
 
 }
